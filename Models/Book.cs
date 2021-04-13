35d5f3aa5d32ceb79e996c3d468b60cbf12c06b2
2020mt93243kairavdevops <2020mt93243kairavdevops@gmail.com>
2021-04-14 00:21:03 +0530

    book model entity added

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,6 +9,17 @@ namespace OSSE_Assignment.Models
 {
     public class Book
     {
-       
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
     }
 }
